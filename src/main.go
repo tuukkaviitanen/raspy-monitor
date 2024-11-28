ade13b17a290637ff057a81ad371e97eba84c5c9
tuukkaviitanen <tuukka.viitanen@gmail.com>
2024-11-28 18:15:31 +0200

    Improved on data gathering

--- src/main.go
+++ src/main.go
@@ -12,6 +12,25 @@ import (
 )
 
 func main() {
+	start := time.Now() // Start timing
+
+	// Should only be fetched once on startup
+	getHostInfo()
+
+	getCpuInfo()
+
+	getMemoryInfo()
+
+	getDiscInfo()
+
+	timestamp := time.Now()
+	fmt.Printf("Timestamp: %s\n", timestamp.Format(time.RFC1123))
+
+	elapsed := time.Since(start) // End timing
+	fmt.Printf("Execution time: %s\n", elapsed)
+}
+
+func getHostInfo() {
 	hostInfo, err := host.Info()
 	if err != nil {
 		fmt.Printf("Error getting host info: %v\n", err)
@@ -25,8 +44,11 @@ func main() {
 	fmt.Printf("Platform Version: %s\n", hostInfo.PlatformVersion)
 	fmt.Printf("Kernel Version: %s\n", hostInfo.KernelVersion)
 	fmt.Printf("Kernel Arch: %s\n", hostInfo.KernelArch)
-	fmt.Printf("Uptime: %d seconds\n", hostInfo.Uptime)
+	fmt.Printf("Boottime: %s\n", time.Unix(int64(hostInfo.BootTime), 0).Format(time.RFC1123))
 
+}
+
+func getCpuInfo() {
 	logicalCount, err := cpu.Counts(true)
 	if err != nil {
 		fmt.Printf("Error getting CPU count: %v\n", err)
@@ -34,17 +56,7 @@ func main() {
 	}
 
 	fmt.Printf("Logical CPU Count: %d\n", logicalCount)
-
 	cpuInterval := 1 * time.Second
-
-	// Get CPU usage percentages for each CPU
-	cpuPercentage, err := cpu.Percent(cpuInterval, true)
-	if err != nil {
-		fmt.Printf("Error getting totatl CPU percentage: %v\n", err)
-		return
-	}
-
-	fmt.Printf("Total CPU Usage: %.2f%%\n", cpuPercentage[0])
 
 	// Get CPU usage percentages for each CPU
 	cpuPercentages, err := cpu.Percent(cpuInterval, true)
@@ -53,15 +65,29 @@ func main() {
 		return
 	}
 
+	// Print total CPU usage (average of all CPUs)
+	totalCPUUsage := 0.0
+	for _, percentage := range cpuPercentages {
+		totalCPUUsage += percentage
+	}
+	totalCPUUsage /= float64(len(cpuPercentages))
+	fmt.Printf("Total CPU Usage: %.2f%%\n", totalCPUUsage)
+
 	// Print CPU usage for each CPU
 	for i, percentage := range cpuPercentages {
 		fmt.Printf("CPU %d: %.2f%%\n", i, percentage)
 	}
+}
+
+func getMemoryInfo() {
 
 	v, _ := mem.VirtualMemory()
 
 	// almost every return value is a struct
 	fmt.Printf("Total: %v, Free:%v, Cached: %v, Buffers: %v, Available: %v, Used: %v, UsedPercent:%f%%\n", v.Total, v.Free, v.Cached, v.Buffers, v.Available, v.Used, v.UsedPercent)
+}
+
+func getTemperatureInfo() {
 
 	// Get and print temperatures
 	temperatures, err := sensors.SensorsTemperatures()
@@ -77,6 +103,9 @@ func main() {
 	if len(temperatures) == 0 {
 		fmt.Println("No temperature sensors found")
 	}
+}
+
+func getDiscInfo() {
 
 	diskUsage, err := disk.Usage("/")
 	if err != nil {
